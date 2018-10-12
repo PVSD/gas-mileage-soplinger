927623f6af84872f4c0a23df16defca9b4552624
soplinger <solekibbles4@gmail.com>
2018-10-11 22:07:21 -0400

    i still winston but i am at home

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,35 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+
+    public int gallons = 0;
+    public int mpg = 0;
+    public int miles = 0;
+
+    public Automobile(int milesPerGallon){
+        mpg = milesPerGallon;
+    }
+
+    public void takeTrip(int miles) {
+        this.miles = miles;
+        gallons = gallons (miles - mpg);
+
+    }
+
+    public void fillUp(int endGallons) {
+        endGallons = endGallons + gallons;
+    }
+
+
+    public void reportFuel() {
+        double fuel = gallons;
+        return fuel;
+    }
+
+
+
+
+
+
+
 }
